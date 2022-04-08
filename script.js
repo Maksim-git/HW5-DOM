099337ada81742db8cb1630628ae859649293e2f
Maksim-git <dv.maksim11@gmail.com>
2022-04-08 14:11:44 +0300

    the location of the notification, and adding an entry to the local storage

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let clickButtion = document.querySelector('#btn-requst').addEventListener('click', ()=>{
-    let userName = document.querySelector('#username').value;
-    let password = document.querySelector('#password').value;
+const clickButtion = document.querySelector('#btn-requst').addEventListener('click', ()=>{
 
- 
+    const userName = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    localStorage.setItem('dataUser', JSON.stringify({Username: userName, Password: password}))
+
     let p = document.createElement('p');
     p.className = "alert";
-    p.innerHTML = "hello";
+    p.innerHTML = 'Username: ' + userName + ', ' + 'Password: ' + password;
+    document.querySelector('.form').append(p);
 
-    document.body.append(p);
-
-
-console.log(p)
-    console.log('username: ' + userName + ', ' + 'password: ' + password)
 })
+
